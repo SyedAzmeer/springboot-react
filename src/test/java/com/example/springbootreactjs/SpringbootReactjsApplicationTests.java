538bd6d65a9c5efcfc13a372d09518693cf3badf
Syed Azmeer <syed.azmeer2111@gmail.com>
2023-05-23 09:52:44 +0800

    First commit

---
+++ src/test/java/com/example/springbootreactjs/SpringbootReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
